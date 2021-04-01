de93d19c2e49c3c0a88a98ffdecd4ecb9f81424c
Tupiet <aleixescrihuela@gmail.com>
2021-04-01 14:01:11 +0200

    Updated the weather

--- Weather/app.js
+++ Weather/app.js
@@ -1,6 +1,9 @@
 const notificationElement = document.querySelector(".notification");
 const iconElement = document.querySelector(".weather-icon");
+const tempDivElement = document.querySelector(".temperature-div");
 const tempElement = document.querySelector(".temperature-value p");
+const maxMinElement = document.querySelector(".temperature-max-min p");
+const humidityElement = document.querySelector(".humidity p");
 const descElement = document.querySelector(".temperature-description p");
 const locationElement = document.querySelector(".location p");
 
@@ -33,8 +36,7 @@ function showError(error) {
 }
 
 function getWeather(latitude, longitude) {
-    let api = `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
-
+    let api = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}`;
     
 
     fetch (api)
@@ -43,7 +45,10 @@ function getWeather(latitude, longitude) {
             return data;
         })
         .then(function(data) {
-            weather.temperature.value = Math.floor(data.main.temp - KELVIN);
+            weather.temperature.value = data.main.temp - KELVIN;
+            weather.temperature.min = data.main.temp_min - KELVIN;
+            weather.temperature.max = data.main.temp_max - KELVIN;
+            weather.temperature.humidity = data.main.humidity;
             weather.description = data.weather[0].description;
             weather.iconId = data.weather[0].icon;
             weather.city = data.name;
@@ -56,7 +61,9 @@ function getWeather(latitude, longitude) {
 
 function displayWeather() {
     iconElement.innerHTML = `<img src="icons/${weather.iconId}.png"/>`;
-    tempElement.innerHTML = `${weather.temperature.value}º<span>C</span>`;
+    tempElement.innerHTML = `${Math.round(weather.temperature.value)}º<span>C</span>`;
+    maxMinElement.innerHTML = `${Math.round(weather.temperature.min)}º<span>C</span> - ${Math.round(weather.temperature.max)}º<span>C</span>`;
+    humidityElement.innerHTML = `${weather.temperature.humidity}% humidity`;
     descElement.innerHTML = weather.description;
     locationElement.innerHTML = `${weather.city}, ${weather.country}`;
 }
@@ -65,17 +72,23 @@ function celsiusToFahrenheit(temperature) {
     return (temperature * 9/5) + 32;
 }
 
-tempElement.addEventListener("click", function() {
+tempDivElement.addEventListener("click", function() {
     if (weather.temperature.value === undefined) return;
 
     if (weather.temperature.unit == "celsius") {
         let fahrenheit = celsiusToFahrenheit(weather.temperature.value);
+        let fahrenheitMin = celsiusToFahrenheit(weather.temperature.min);
+        let fahrenheitMax = celsiusToFahrenheit(weather.temperature.max);
         fahrenheit = Math.floor(fahrenheit);
+        fahrenheitMin = Math.round(fahrenheitMin);
+        fahrenheitMax = Math.round(fahrenheitMax);
 
         tempElement.innerHTML = `${fahrenheit}º<span>F</span>`;
+        maxMinElement.innerHTML = `${fahrenheitMin}º<span>F</span> - ${fahrenheitMax}º<span>F</span>`;
         weather.temperature.unit = "fahrenheit";
     } else {
-        tempElement.innerHTML = `${weather.temperature.value}º<span>C</span>`;
+        tempElement.innerHTML = `${Math.round(weather.temperature.value)}º<span>C</span>`;
+        maxMinElement.innerHTML = `${Math.round(weather.temperature.min)}º<span>C</span> - ${Math.round(weather.temperature.max)}º<span>C</span>`;
         weather.temperature.unit = "celsius";
     }
 });
